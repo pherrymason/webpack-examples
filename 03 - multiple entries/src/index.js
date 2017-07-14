3504e61a2cd68df76665f0aafca6b49261250ca7
Raúl Ferràs <raul@viscaweb.com>
2017-07-14 12:53:49 +0200

    Added 3 simple examples

---
+++ 03 - multiple entries/src/index.js
@@ -0,0 +1,4 @@
+import fooString from "./foo.js";
+var $ = require('jquery');
+
+console.log('HELLO ' + fooString + ' WEBPACK');
---
+++ 03 - multiple entries/webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+    entry: {
+        index: "./src/index.js",
+        index2: "./src/index2.js"
+    },
+    output: {
+        filename: '[name].js',
+        path: path.join(__dirname, 'build')
+    }
+};
